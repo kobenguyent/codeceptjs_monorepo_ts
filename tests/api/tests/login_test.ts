9a61a32119e7e289d0ff26e9b45d7b4067b8bfeb
kobenguyent <kobenguyent@users.noreply.github.com>
2025-05-22 08:48:03 +0000

    Automated deployment: Thu May 22 08:48:03 UTC 2025 4d9b9356d0515a44ba4dcd4362f2217585f4e28e

--- tests/api/tests/login_test.ts
+++
@@ -1,9 +0,0 @@
-import {userApi} from "../support/api/userApi";
-
-Feature('API Login');
-
-Scenario('Login via API returns token', async ({ I }) => {
-  const response = await userApi.login('emilys', 'emilyspass');
-  I.seeResponseCodeIsSuccessful();
-  I.seeResponseContainsKeys(['accessToken']);
-});
